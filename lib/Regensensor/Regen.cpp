487f6f99073d2006bc1de40ece784669d77ebe78
Niviera <113305331+Niviera@users.noreply.github.com>
2023-06-23 09:56:22 +0200

    Start: Regensensoren

---
+++ lib/Regensensor/Regen.cpp
@@ -0,0 +1,17 @@
+/* Libarys */
+#include <Arduino.h>
+#include "Regen.h"
+
+/* Allgemeine Funtkionen */
+
+/* Getter & Setter */
+
+/* Konstruktoren & Destructoren */
+Regen::Regen(int pin)
+{
+    this->pin = pin;
+}
+
+Regen::~Regen()
+{
+}
---
+++ lib/Regensensor/Regen.h
@@ -0,0 +1,15 @@
+class Regen
+{
+private:
+    int pin;
+    bool aktuell_Regen;
+    int fuzzy_Regen;
+
+public:
+    /* Allgemeine Funktionen */
+    
+    /* Getter & Setter */
+    /* Konstrukor & Destructor */
+    Regen(int pin);
+    ~Regen();
+};
